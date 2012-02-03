724ee5d3a38184d643078c979140d833dbe48827
Sarah Elliott <selliott@willowgarage.com>
2012-02-03 13:17:10 -0800

    Add

---
+++ app_manager/msg_gen/lisp/_package.lisp
@@ -0,0 +1,23 @@
+(cl:defpackage app_manager-msg
+  (:use )
+  (:export
+   "<APP>"
+   "APP"
+   "<STATUSCODES>"
+   "STATUSCODES"
+   "<KEYVALUE>"
+   "KEYVALUE"
+   "<APPSTATUS>"
+   "APPSTATUS"
+   "<APPLIST>"
+   "APPLIST"
+   "<CLIENTAPP>"
+   "CLIENTAPP"
+   "<APPINSTALLATIONSTATE>"
+   "APPINSTALLATIONSTATE"
+   "<ICON>"
+   "ICON"
+   "<EXCHANGEAPP>"
+   "EXCHANGEAPP"
+  ))
+
