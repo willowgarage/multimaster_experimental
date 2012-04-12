b2a1400f3534daebc1e08c523706924c07723850
Ken Conley <kwc@willowgarage.com>
2012-04-12 15:12:42 -0700

    porting to Fuerte APIs

--- app_manager_android/scripts/show_master_qrcode.py
+++ app_manager_android/scripts/show_master_qrcode.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python
 import roslib; roslib.load_manifest('app_manager_android')
-import roslib.network
-import roslib.rosenv
+import rosgraph
+import rosgraph.network
 import pyqrnative
 
 import urlparse
-urlp = urlparse.urlparse(roslib.rosenv.get_master_uri())
-address = roslib.network.get_local_address()
+urlp = urlparse.urlparse(rosgraph.get_master_uri())
+address = rosgraph.network.get_local_address()
 
 text = "http://%s:%s"%(address, urlp.port)
 print text
